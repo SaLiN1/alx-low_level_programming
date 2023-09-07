6ca2e658eddd30c212a136ffbb896e3a91973bcf
SaLiN1 <safa_algerie@yahoo.com>
2023-09-07 14:43:00 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0; }
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that print withputs fonction
+ * Return: Always to 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
