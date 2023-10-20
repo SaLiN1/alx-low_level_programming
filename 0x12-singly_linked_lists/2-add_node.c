a64faba6290d6301627b6166606877b607959a2f
SaLiN1 <safa_algerie@yahoo.com>
2023-10-20 07:47:35 -0700

    t2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,37 +6,22 @@
  * @str: str field of node.
  * Return: address of the head.
 */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new;
+	unsigned int i = 0;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node != NULL)
-	{
-		new_node->str = strdup(str);
-		new_node->len = _strlen(str);
-		new_node->next = *head;
-	}
-	else
-		return (NULL);
-	if (*head != NULL)
-		new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-}
-
-/**
- * _strlen - counts the length of a string
- * @str: string to be counted
- *
- * Return: the length of the string
- */
-int _strlen(const char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (str[i])
 		i++;
-	return (i);
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = i;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
 }
