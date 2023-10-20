859abefd2220d580878b7f7e4a2f596a985ffdfa
SaLiN1 <safa_algerie@yahoo.com>
2023-10-20 07:34:04 -0700

    task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,24 +9,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_head;
+	size_t i;
 
-	new_head = malloc(sizeof(list_t));
+	list_t *new_head = malloc(sizeof(list_t));
 
-	if (!head || !new_head)
+	if (new == NULL)
 		return (NULL);
-	if (str)
-	{
-		new_head->str = strdup(str);
-		if (!new_head->str)
-		{
-			free(new_head);
-			return (NULL);
-		}
-		new_head->len = _strlen(new_head->str);
-	}
 
-	new_head->next = *head;
-	*head = new_head;
-	return (new_head);
+	new->str = strdup(str);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	new->len = i;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
 }
