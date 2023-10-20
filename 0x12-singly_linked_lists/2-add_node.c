e59f880340ca60c90ada36638a7e676347913bcf
SaLiN1 <safa_algerie@yahoo.com>
2023-10-20 08:02:53 -0700

    t2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,20 +8,28 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int i = 0;
+	if (str == NULL || str[0] == '\0' || strlen(str) > MAX_STRING_LENGTH)
+		return (NULL);
 
-	while (str[i])
-		i++;
+	list_t *new;
+	unsigned int i = strlen(str);
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+
+	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
 	new->len = i;
 	new->next = (*head);
 	(*head) = new;
 
 	return (*head);
 }
+
