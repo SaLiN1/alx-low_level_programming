76dc59819b60a88255b2d893a614a346d87a5133
SaLiN1 <safa_algerie@yahoo.com>
2023-10-30 10:58:38 -0700

    t1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int E, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,9 +22,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	E = write(o, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (o == -1 || E == -1)
 		return (-1);
 
 	close(o);
